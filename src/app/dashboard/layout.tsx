97537791a7db3918e51128393d0ebff0b0f670fe
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-12-24 14:14:15 -0800

    add flex for collapsed sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,28 @@
+"use client";
+import { useState } from "react";
 import Sidebar from "../components/Sidebar";
+import { LayoutContext } from "../hooks/useContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+  console.log(isCollapsed, "from laupt");
+
   return (
-    <div className="grid grid-cols-11  ">
-      <div className="col-auto md:col-span-3 hidden md:block">
-        <Sidebar />
+    <LayoutContext.Provider value={{ isCollapsed, setIsCollapsed }}>
+      <div className="flex">
+        <div
+          className={`md:w-3/12 w-0 ${
+            isCollapsed
+              ? "flex-shrink-0 flex-grow-0 basis-full transition-all duration-150"
+              : ""
+          }`}
+        >
+          <Sidebar />
+        </div>
+        <div className={`md:w-9/12 w-full ${isCollapsed ? "expanded" : ""}`}>
+          {children}
+        </div>
       </div>
-      <div className="col-span-full md:col-span-8">{children}</div>
-    </div>
+    </LayoutContext.Provider>
   );
 }
